84fe12a7336e5e95fb4cb8ecfbb65454f66392f9
Luis-Gerardo <webdesigner@gerardobarroso.com>
2018-11-16 10:31:57 -0800

    Change class to funtional component passing props using spread operator

--- src/Components/Names.js
+++ src/Components/Names.js
@@ -1,31 +1,26 @@
-import React, { Component } from 'react'
+import React from 'react'
 import Name from './Name'
 
-class Names extends Component {
+const Names = props => {
 
-    render() {
-        
-        const { onUpdate, onEdit, onCheck, onRemove, people } = this.props
+    const { onUpdate, onEdit, onCheck, onRemove, people } = props
 
-        const list = people.map((person, index) => {
-            return <Name 
-                key={index} 
-                onRemove={(e) => {onRemove(e,index)}} 
-                onCheck={(e) => {onCheck(e,index)}} 
-                onEdit={(e) => {onEdit(e,index)}} 
-                onUpdate={(e) => {onUpdate(e,index)}} 
-                name={person.name} 
-                age={person.age}
-                edit={person.edit}
-                check={person.check} />
-        })
+    const list = people.map((person, index) => {
+        return <Name 
+            key={index} 
+            onRemove={(e) => {onRemove(e,index)}} 
+            onCheck={(e) => {onCheck(e,index)}} 
+            onEdit={(e) => {onEdit(e,index)}} 
+            onUpdate={(e) => {onUpdate(e,index)}} 
+            // Pass All props instead of using name={person.name}
+            {...person} />
+    })
 
-        return (
-            <div>
-                { people.length === 0 ? <p className="alert alert-danger" role="alert">No Info</p> : <ul className="list-group">{list}</ul>}
-            </div>
-        )
-    }
+    return (
+        <div>
+            { people.length === 0 ? <p className="alert alert-danger" role="alert">No Info</p> : <ul className="list-group">{list}</ul>}
+        </div>
+    )
 }
 
 export default Names
